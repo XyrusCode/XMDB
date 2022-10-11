5e9df5abd48bf4d3b09d2645d81ae2c12018afb8
Prince Shammah <pshammah@epohcj.com>
2022-10-11 16:58:45 +0100

    added advanced sw

---
+++ public/sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"abcfdf8633754fdbae45f08998b507db","url":"index.html"},{"revision":"c2a598523864bfd2bfd5e57689983e55","url":"offline.html"},{"revision":"0e8f96ab4e585a2f09a561bee0adbb06","url":"serviceWorker.js"}]);
