380ca7e7856bf3826dca9fbf8a453672708a182f
Prince Shammah <pshammah@epohcj.com>
2022-10-11 16:35:20 +0100

    Updated service worker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,141 +1,53 @@
-'use strict';
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-/**
- * Service Worker of PWA
- * To learn more and add one to your website, visit - https://PWA.com
- */
- 
-const cacheName = 'xmdb-cache';
-const startPage = 'https://xmdb.netlify.app';
-const offlinePage = 'https://xmdb.netlify.app/';
-const filesToCache = [startPage, offlinePage ];
+const CACHE = 'xmdb-cache';
 
-// Install
-self.addEventListener('install', function(e) {
-	console.log('PWA service worker installation');
-	e.waitUntil(
-		caches.open(cacheName).then(function(cache) {
-			console.log('PWA service worker caching dependencies');
-			filesToCache.map(function(url) {
-				return cache.add(url).catch(function (reason) {
-					return console.log('PWA: ' + String(reason) + ' ' + url);
-				});
-			});
-		})
-	);
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const offlineFallbackPage = 'https://xmdb.netlify.app/';
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-// Activate
-self.addEventListener('activate', function(e) {
-	console.log('PWA service worker activation');
-	e.waitUntil(
-		caches.keys().then(function(keyList) {
-			return Promise.all(keyList.map(function(key) {
-				if ( key !== cacheName ) {
-					console.log('PWA old cache removed', key);
-					return caches.delete(key);
-				}
-			}));
-		})
-	);
-	return self.clients.claim();
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
 });
 
-// Range Data Code
-var fetchRangeData = function(event){
-    var pos = Number(/^bytes\=(\d+)\-$/g.exec(event.request.headers.get('range'))[1]);
-            console.log('Range request for', event.request.url, ', starting position:', pos);
-            event.respondWith(
-              caches.open(cacheName)
-              .then(function(cache) {
-                return cache.match(event.request.url);
-              }).then(function(res) {
-                if (!res) {
-                  return fetch(event.request)
-                  .then(res => {
-                    return res.arrayBuffer();
-                  });
-                }
-                return res.arrayBuffer();
-              }).then(function(ab) {
-                return new Response(
-                  ab.slice(pos),
-                  {
-                    status: 206,
-                    statusText: 'Partial Content',
-                    headers: [
-                      // ['Content-Type', 'video/webm'],
-                      ['Content-Range', 'bytes ' + pos + '-' +
-                        (ab.byteLength - 1) + '/' + ab.byteLength]]
-                  });
-              }));
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
 }
 
-// Fetch
-self.addEventListener('fetch', function(e) {
-	
-	// Return if the current request url is in the never cache list
-	if ( ! neverCacheUrls.every(checkNeverCacheList, e.request.url) ) {
-	  console.log( 'PWA: Current request is excluded from cache.' );
-	  return;
-	}
-	
-	// Return if request url protocal isn't http or https
-	if ( ! e.request.url.match(/^(http|https):\/\//i) )
-		return;
-	
-           // For Range Headers
-    if (e.request.headers.get('range')) {
-            fetchRangeData(e);
-        } else {
-			// For POST requests, do not use the cache. Serve offline page if offline.
-			if ( e.request.method !== 'GET' ) {
-				e.respondWith(
-					fetch(e.request).catch( function() {
-						        return caches.match(offlinePage);
-					})
-				);
-				return;
-			}
-			
-			// Revving strategy
-			if ( e.request.mode === 'navigate' && navigator.onLine ) {
-				e.respondWith(
-					fetch(e.request).then(function(response) {
-						return caches.open(cacheName).then(function(cache) {
-							cache.put(e.request, response.clone());
-							return response;
-						});  
-					})
-				);
-				return;
-			}
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
 
-			e.respondWith(
-				caches.match(e.request).then(function(response) {
-					return response || fetch(e.request).then(function(response) {
-						return caches.open(cacheName).then(function(cache) {
-							cache.put(e.request, response.clone());
-							return response;
-						});  
-					});
-				}).catch(function() {
-					return caches.match(offlinePage);
-				})
-			);
-   }
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
-
-// Check if current url is in the neverCacheUrls list
-function checkNeverCacheList(url) {
-	if ( this.match(url) ) {
-		return false;
-	}
-	return true;
-}
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js");
-	            if(workbox.googleAnalytics){
-                  try{
-                    workbox.googleAnalytics.initialize();
-                  } catch (e){ console.log(e.message); }
-                }
